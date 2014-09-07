e6b8f946b4c3dee94dd6085a1da815b1a7384acb
charliedowler <charliedowler15@gmail.com>
2014-09-07 21:45:04 +0100

    indentation tweaks

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 var through = require('through2'),
-	gutil = require('gulp-util'),
-	crypto = require('crypto');
+  gutil = require('gulp-util'),
+  crypto = require('crypto');
 
 module.exports = function (param) {
-	'use strict';
+  'use strict';
 
-	// if necessary check for required param(s), e.g. options hash, etc.
-	if (!param) {
-		throw new gutil.PluginError('gulp-encrypt', 'No param supplied');
-	}
-	else if (!param.key) {
-		throw new gutil.PluginError('gulp-encrypt', 'No key param supplied');
-	}
+  // if necessary check for required param(s), e.g. options hash, etc.
+  if (!param) {
+    throw new gutil.PluginError('gulp-encrypt', 'No param supplied');
+  }
+  else if (!param.key) {
+    throw new gutil.PluginError('gulp-encrypt', 'No key param supplied');
+  }
 
-	// see 'Writing a plugin'
-	// https://github.com/gulpjs/gulp/blob/master/docs/writing-a-plugin/README.md
-	function gulpEncrypt(file, enc, callback) {
-		/*jshint validthis:true*/
+  // see 'Writing a plugin'
+  // https://github.com/gulpjs/gulp/blob/master/docs/writing-a-plugin/README.md
+  function gulpEncrypt(file, enc, callback) {
+    /*jshint validthis:true*/
 
-		// Do nothing if no contents
-		if (file.isNull()) {
-			this.push(file);
-			return callback();
-		}
+    // Do nothing if no contents
+    if (file.isNull()) {
+      this.push(file);
+      return callback();
+    }
 
-		if (file.isStream()) {
+    if (file.isStream()) {
 
-			// http://nodejs.org/api/stream.html
-			// http://nodejs.org/api/child_process.html
-			// https://github.com/dominictarr/event-stream
+      // http://nodejs.org/api/stream.html
+      // http://nodejs.org/api/child_process.html
+      // https://github.com/dominictarr/event-stream
 
-			// accepting streams is optional
-			this.emit('error',
-				new gutil.PluginError('gulp-encrypt', 'Stream content is not supported'));
-			return callback();
-		}
+      // accepting streams is optional
+      this.emit('error',
+        new gutil.PluginError('gulp-encrypt', 'Stream content is not supported'));
+      return callback();
+    }
 
-		// check if file.contents is a `Buffer`
-		if (file.isBuffer()) {
+    // check if file.contents is a `Buffer`
+    if (file.isBuffer()) {
 
-			// manipulate buffer in some way
-			// http://nodejs.org/api/buffer.html
+      // manipulate buffer in some way
+      // http://nodejs.org/api/buffer.html
       var contents = file.contents.toString();
       var key = param.key;
 
@@ -54,12 +54,12 @@ module.exports = function (param) {
         decipher.update(contents, 'base64', 'utf8');
         contents = decipher.final('utf8');
       }
-			file.contents = new Buffer(contents);
+      file.contents = new Buffer(contents);
 
-			this.push(file);
-		}
-		return callback();
-	}
+      this.push(file);
+    }
+    return callback();
+  }
 
-	return through.obj(gulpEncrypt);
+  return through.obj(gulpEncrypt);
 };
